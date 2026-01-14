c91da8876c4ed6616ba7960e1afe875bec02795b
Darren <q0981516608@gmail.com>
2026-01-14 10:57:00 +0800

    perf: 移除每五分鐘獲取 Redis Cache Key , 後台已經可以清楚看到全部的 Key

--- src/main/java/com/next/nexrailai/config/AppConfig.java
+++ src/main/java/com/next/nexrailai/config/AppConfig.java
@@ -5,7 +5,6 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.ChatMemoryRepository;
-import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.chat.memory.repository.jdbc.PostgresChatMemoryRepositoryDialect;
